1c518db74da60a679554322e939496952b12f757
jagruthi35 <114712771+jagruthi35@users.noreply.github.com>
2023-06-20 22:21:22 +0530

    Update custom_script.sh

--- custom_script.sh
+++ custom_script.sh
@@ -9,25 +9,28 @@ sudo apt-get -y install gunicorn
 sudo apt-get -y install nginx
 sudo apt-get -y install git
 git clone https://github.com/jagruthi35/miniproject-practice.git
-
-sudo touch "/etc/nginx/sites-enabled/miniproject-practice"
-file_path="/etc/nginx/sites-enabled/miniproject-practice"
-ip_address=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-echo "private_ip: $ip_address"
-# Define the content
-content='server {
+private_ip_address=$(hostname -I | awk '{print $1}')
+sudo tee /etc/nginx/sites-enabled/miniproject-practice >/dev/null <<EOT
+server {
     listen 80;
     server_name 20.114.206.65;
 
     location / {
-        proxy_pass http://$ip_address:8000;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
+        proxy_pass http://$private_ip_address:8000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
     }
-}'
-echo "$content" > "$file_path"
-sudo unlink /etc/nginx/sites-enabled/default
-sudo nginx -s reload
-sudo service nginx restart
+}
+EOT
+
+# Remove the default NGINX configuration
+sudo rm -f /etc/nginx/sites-enabled/default
+
+# Restart NGINX
+sudo systemctl restart nginx
+
+# Navigate to the cloned repository
 cd miniproject-practice
-gunicorn -b $ip_address main:app
+
+# Start the Flask application using Gunicorn
+gunicorn -b $private_ip_address:8000 main:app
