b7daadde271f15e8c3df37251114af7d68be0dfa
jagruthi35 <114712771+jagruthi35@users.noreply.github.com>
2023-06-23 01:18:49 +0530

    Delete backend.tf

--- terraform35/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "practise-project35"
-    storage_account_name = "terraform35"
-    container_name       = "terraformbackend"
-    key                  = "terraformbackend"
-    access_key           = "hukigfHyh6CyIBS7cOXboJ2OZSbPX1BfbPqm9Qt8V8DU70TQfy1UvxvfDEb3u03IArQDacf4OOzg+ASteGutiw=="
-  }
-}
