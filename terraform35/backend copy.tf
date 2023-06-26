a816c5b9fadad04e98dd1086d076db63ddc8cc6f
jagruthi35 <114712771+jagruthi35@users.noreply.github.com>
2023-06-26 17:09:12 +0530

    Update backend copy.tf

--- terraform35/backend copy.tf
+++ terraform35/backend copy.tf
@@ -4,6 +4,5 @@ terraform {
     storage_account_name = "terraform35"
     container_name       = "terraformbackend"
     key                  = "terraformbackend"
-    access_key           = "hukigfHyh6CyIBS7cOXboJ2OZSbPX1BfbPqm9Qt8V8DU70TQfy1UvxvfDEb3u03IArQDacf4OOzg+ASteGutiw=="
   }
 }
