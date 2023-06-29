4c2738a616c43edb5316109b382bf2e7a56d7692
jagruthi35 <114712771+jagruthi35@users.noreply.github.com>
2023-06-29 16:43:38 +0530

    Delete terraform35 directory

--- terraform35/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "practise-project35"
-    storage_account_name = "terraform35"
-    container_name       = "terraformbackend"
-    key                  = "terraformbackend"
-  }
-}
